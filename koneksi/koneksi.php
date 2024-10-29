a45d05fe1d7488a485ad381c09b61b7873257b27
Naufal Fahmi <naufalffadhillah@gmail.com>
2024-10-29 21:25:19 +0700

    revisi_1

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,26 +1,12 @@
 <?php
-/*
-  | Source Code Aplikasi Rental Mobil PHP & MySQL
-  | 
-  | @package   : rental_mobil
-  | @file	   : koneksi.php 
-  | @author    : fauzan1892 / Fauzan Falah
-  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
-  | @blog      : https://www.codekop.com/products/source-code-aplikasi-rental-mobil-php-mysql-7.html 
-  | 
-  | 
-  | 
-  | 
- */
-    $user = 'pale';
-    $pass = 'pale';
+    $user = 'rds_admin';
+    $pass = '48F8B31Fe576';
 
-    $koneksi = new PDO("mysql:host=localhost:3307;dbname=penyewaan_studio", $user, $pass);
+    $koneksi = new PDO("mysql:rm-d9jetkk94383ff035.mysql.ap-southeast-5.rds.aliyuncs.com:3306;dbname=penyewaan_studio", $user, $pass);
 
     global $url;
-    $url = "http://localhost:8080/sewa_studio/";
+    $url = "8.215.4.20";
 
-    
     $sql_web = "SELECT * FROM studio WHERE id_studio = 5";
     $row_web = $koneksi->prepare($sql_web);
     $row_web->execute();
